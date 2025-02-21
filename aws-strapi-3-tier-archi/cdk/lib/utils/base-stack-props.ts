c82dea762375bdd1f50cf511ba23d9b235d387de
phtre57 <phtre57@gmail.com>
2025-02-21 14:35:21 -0500

    fix typing

--- aws-strapi-3-tier-archi/cdk/lib/utils/base-stack-props.ts
+++ aws-strapi-3-tier-archi/cdk/lib/utils/base-stack-props.ts
@@ -3,7 +3,7 @@ import { Config } from '../../cdk-config-definition'
 
 export interface IBaseStackProps extends cdk.StackProps {
   readonly env?: cdk.Environment
-  readonly envName: 'dev' | 'prod'
+  readonly envName: Config.EnvName
   readonly region: string
   readonly envConfig: Config.IEnvConfig
 }
